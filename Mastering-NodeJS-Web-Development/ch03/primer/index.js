d43dc0a752064352f5e7a5ea1ddfac5eb95217f9
David Stevenson <drstevenson1958@yahoo.com>
2024-09-14 11:45:08 -0400

    Listing 3.35 Defining an optional parameter in the index.ts file in the primer folder

--- Mastering-NodeJS-Web-Development/ch03/primer/index.js
+++ Mastering-NodeJS-Web-Development/ch03/primer/index.js
@@ -1,11 +1,11 @@
 "use strict";
-// Listing 3.34 Defining a function in the index.ts file in the primer folder
+// Listing 3.35 Defining an optional parameter in the index.ts file in the primer folder
 Object.defineProperty(exports, "__esModule", { value: true });
 function writeValue(val) {
     console.log(`Value: ${val ?? "Fallback value"}`);
 }
 writeValue("London");
-writeValue(null);
+writeValue();
 // Output
 // Value: London
 // Value: Fallback value
