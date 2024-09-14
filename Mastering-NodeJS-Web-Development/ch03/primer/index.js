0d72f5755c822ba6341ec48e9ce8908c26f04637
David Stevenson <drstevenson1958@yahoo.com>
2024-09-14 11:18:07 -0400

    Listing 3.24 Assigning null and undefined values in the index.ts file in the primer folder

--- Mastering-NodeJS-Web-Development/ch03/primer/index.js
+++ Mastering-NodeJS-Web-Development/ch03/primer/index.js
@@ -1,6 +1,5 @@
 "use strict";
-// Listing 3.23 Defining number values in the index.ts file in the primer folder
+// Listing 3.24 Assigning null and undefined values in the index.ts file in the primer folder
 Object.defineProperty(exports, "__esModule", { value: true });
-let daysInWeek = 7;
-let pi = 3.14;
-let hexValue = 0xFFFF;
+let person1 = "Alice";
+let person2 = "Bob";
