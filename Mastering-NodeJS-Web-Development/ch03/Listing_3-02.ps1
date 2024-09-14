d567737a64f9c1e30161e26625e1f2ca5b01abe2
David Stevenson <drstevenson1958@yahoo.com>
2024-09-14 09:57:40 -0400

    Listing 3.2. Installng the development packages
    npm install nodemon@2.0.20
    npm install tsc-watch@6.0.4
    npm install typescript@5.2.2
    npm install @tsconfig/node20@20.1.4
    npm install @types/node@20.6.1

---
+++ Mastering-NodeJS-Web-Development/ch03/Listing_3-02.ps1
@@ -0,0 +1,7 @@
+# Listing 3.2. Installng the development packages
+
+npm install nodemon@2.0.20
+npm install tsc-watch@6.0.4
+npm install typescript@5.2.2
+npm install @tsconfig/node20@20.1.4
+npm install @types/node@20.6.1
