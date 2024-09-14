02ae4e3caa8dae50b8fc55398258660f2315b81f
David Stevenson <drstevenson1958@yahoo.com>
2024-09-14 09:33:34 -0400

    Listing 2.12 Setting the path in PowerShell
    $env:path += ';\node_modules\.bin'
    
    $env:path.split(';')

---
+++ Mastering-NodeJS-Web-Development/ch02/Listing_2-12.ps1
@@ -0,0 +1,66 @@
+# Listing 2.12 Setting the path in PowerShell
+
+$env:path += ';\node_modules\.bin' 
+
+$env:path.split(';')
+
+# C:\Program Files\PowerShell\7
+# C:\Program Files\Java\jdk-20\bin
+# C:\Program Files\Python311\Scripts\
+# C:\Program Files\Python311\
+# C:\Program Files\Microsoft MPI\Bin\
+# C:\OracleSoftwares\bin
+# C:\WINDOWS\system32
+# C:\WINDOWS
+# C:\WINDOWS\System32\Wbem
+# C:\WINDOWS\System32\WindowsPowerShell\v1.0\
+# C:\Program Files\Microsoft SQL Server\Client SDK\ODBC\110\Tools\Binn\
+# C:\Program Files (x86)\Microsoft SQL Server\120\DTS\Binn\
+# C:\Users\drste\.dnx\bin
+# C:\Program Files\Microsoft DNX\Dnvm\
+# C:\Program Files\dotnet\
+# C:\Program Files\TortoiseHg\
+# C:\WINDOWS\System32\OpenSSH\
+# C:\Users\drste\AppData\Roaming\nvm
+# C:\Program Files\nodejs
+# C:\Program Files\Microsoft VS Code\bin
+# C:\Program Files (x86)\Microsoft SQL Server\130\DTS\Binn\
+# C:\Program Files\Microsoft SQL Server\Client SDK\ODBC\130\Tools\Binn\
+# C:\Program Files (x86)\Microsoft SQL Server\140\DTS\Binn\
+# C:\Program Files\Microsoft SQL Server\Client SDK\ODBC\170\Tools\Binn\
+# C:\Program Files (x86)\Microsoft SQL Server\150\DTS\Binn\
+# C:\ProgramData\chocolatey\bin
+# C:\Program Files\Microsoft SQL Server\140\DTS\Binn\
+# C:\Program Files\Azure Data Studio\bin
+# C:\Program Files\Microsoft SQL Server\150\Tools\Binn\
+# C:\Program Files (x86)\Microsoft SQL Server\110\DTS\Binn\
+# C:\Program Files (x86)\Microsoft SQL Server\160\DTS\Binn\
+# C:\Program Files\Microsoft SQL Server\150\DTS\Binn\
+# C:\Program Files (x86)\Microsoft SQL Server\150\Tools\Binn\
+# C:\Program Files (x86)\Microsoft SQL Server\160\Tools\Binn\
+# C:\Program Files\Microsoft SQL Server\160\Tools\Binn\
+# C:\Program Files\Microsoft SQL Server\160\DTS\Binn\
+# C:\Program Files\Microsoft SQL Server\130\Tools\Binn\
+# C:\Program Files\GitHub CLI\
+# C:\Program Files\Git\cmd
+# C:\Program Files\Docker\Docker\resources\bin
+# C:\Program Files\nodejs\
+# C:\Program Files\PowerShell\7\
+# C:\Program Files\MySQL\MySQL Shell 8.0\bin\
+# C:\Users\drste\AppData\Local\Programs\Python\Python311\Scripts\
+# C:\Users\drste\AppData\Local\Programs\Python\Python311\
+# C:\Users\drste\AppData\Local\Microsoft\WindowsApps
+# C:\Program Files\Microsoft VS Code\bin
+# C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python36_64
+# C:\Users\drste\AppData\Roaming\nvm
+# C:\Program Files\nodejs
+# C:\Users\drste\AppData\Local\GitHubDesktop\bin
+# C:\Users\drste\.dotnet\tools
+# C:\Users\drste\AppData\Local\Programs\Fiddler
+# C:\Users\drste\AppData\Local\Microsoft\WindowsApps
+# C:\Program Files\Azure Data Studio\bin
+# C:\Users\drste\.dotnet\tools
+# C:\Program Files\mongosh\
+# C:\Users\drste\AppData\Local\Microsoft\WinGet\Packages\Schniz.fnm_Microsoft.Winget.Source_8wekyb3d8bbwe        
+# C:\Users\drste\AppData\Roaming\npm
+# \node_modules\.bin
