29e46a54566b79b79e46791440a81629ffd07e17
David Stevenson <drstevenson1958@yahoo.com>
2024-09-14 10:44:19 -0400

    Listing 3.17 Defining a variable without a value in the index.ts file in the primer folder

--- Mastering-NodeJS-Web-Development/ch03/primer/index.js
+++ Mastering-NodeJS-Web-Development/ch03/primer/index.js
@@ -1,6 +1,13 @@
 "use strict";
-// Listing 3.16 Specifying types in the index.ts file in the primer folder
+// Listing 3.17 Defining a variable without a value in the index.ts file in the primer folder
 Object.defineProperty(exports, "__esModule", { value: true });
 let condition = true;
 let person = "Bob";
 const age = 40;
+let place;
+console.log("Place value: " + place + " Type: " + typeof (place));
+place = "London";
+console.log("Place value: " + place + " Type: " + typeof (place));
+// Output
+// Place value: undefined Type: undefined
+// Place value: London Type: string
