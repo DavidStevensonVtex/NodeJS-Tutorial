3f4362f3e8cfc128f620b2b09b69c75e199146bd
David Stevenson <drstevenson1958@yahoo.com>
2024-09-14 11:20:58 -0400

    Listing 3.25 Using the if/else and switch conditional statements in the index.ts file in the primer folder

--- Mastering-NodeJS-Web-Development/ch03/primer/index.js
+++ Mastering-NodeJS-Web-Development/ch03/primer/index.js
@@ -1,5 +1,27 @@
 "use strict";
-// Listing 3.24 Assigning null and undefined values in the index.ts file in the primer folder
+// Listing 3.25 Using the if/else and switch conditional statements in the index.ts file in the primer folder
 Object.defineProperty(exports, "__esModule", { value: true });
-let person1 = "Alice";
-let person2 = "Bob";
+let firstName = "Adam";
+if (firstName == "Adam") {
+    console.log("firstName is Adam");
+}
+else if (firstName == "Jacqui") {
+    console.log("firstName is Jacqui");
+}
+else {
+    console.log("firstName is neither Adam or Jacqui");
+}
+switch (firstName) {
+    case "Adam":
+        console.log("firstName is Adam");
+        break;
+    case "Jacqui":
+        console.log("firstName is Jacqui");
+        break;
+    default:
+        console.log("firstName is neither Adam or Jacqui");
+        break;
+}
+// Output
+// firstName is Adam
+// firstName is Adam
