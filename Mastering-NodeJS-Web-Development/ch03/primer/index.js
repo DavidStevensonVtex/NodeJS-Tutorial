aaba752f67db46363ab4eac6b8b339f52328cb53
David Stevenson <drstevenson1958@yahoo.com>
2024-09-14 10:46:15 -0400

    Listing 3.18 Assigning null in the index.ts file in the primer folder
    Place value: undefined Type: undefined
    Place value: London Type: string
    Place value: null Type: object

--- Mastering-NodeJS-Web-Development/ch03/primer/index.js
+++ Mastering-NodeJS-Web-Development/ch03/primer/index.js
@@ -1,5 +1,5 @@
 "use strict";
-// Listing 3.17 Defining a variable without a value in the index.ts file in the primer folder
+// Listing 3.18 Assigning null in the index.ts file in the primer folder
 Object.defineProperty(exports, "__esModule", { value: true });
 let condition = true;
 let person = "Bob";
@@ -8,6 +8,9 @@ let place;
 console.log("Place value: " + place + " Type: " + typeof (place));
 place = "London";
 console.log("Place value: " + place + " Type: " + typeof (place));
+place = null;
+console.log("Place value: " + place + " Type: " + typeof (place));
 // Output
 // Place value: undefined Type: undefined
 // Place value: London Type: string
+// Place value: null Type: object  
