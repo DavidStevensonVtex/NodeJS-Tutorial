71733c8c76f3d882d7f578ba50b0fcaf09e70a72
David Stevenson <drstevenson1958@yahoo.com>
2024-09-14 09:28:24 -0400

    Listing 2.10 Checking for package security vulnerabilities
    npm install --save-dev nodemon@2.0.20
    
    # 3 high severity vulnerabilities
    
    npm audit
    
    # 3 high severity vulnerabilities

---
+++ Mastering-NodeJS-Web-Development/ch02/Listing_2-10.ps1
@@ -0,0 +1,9 @@
+# Listing 2.10 Checking for package security vulnerabilities
+
+npm install --save-dev nodemon@2.0.20
+
+# 3 high severity vulnerabilities
+
+npm audit
+
+# 3 high severity vulnerabilities
