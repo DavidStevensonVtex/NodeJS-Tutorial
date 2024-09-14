42ef09d6d4287e93c496277e0be54fb848301778
David Stevenson <drstevenson1958@yahoo.com>
2024-09-14 10:18:10 -0400

    Listing 3.10 Starting the TypeScript tools
    npm run use_ts

---
+++ Mastering-NodeJS-Web-Development/ch03/Listing_3-10.ps1
@@ -0,0 +1,6 @@
+# Listing 3.10 Starting the TypeScript tools
+
+npm run use_ts
+
+# Output
+# index.ts(10,18): error TS2345: Argument of type 'string' is not assignable to parameter of type 'number'.
--- Mastering-NodeJS-Web-Development/ch03/primer/index.js
+++ Mastering-NodeJS-Web-Development/ch03/primer/index.js
@@ -1,22 +1,11 @@
-// Listing 3.8 Checking types in the index.js file in the primer folder
-
+"use strict";
+// Listing 3.9 The contents of the index.ts file in the primer folder
+Object.defineProperty(exports, "__esModule", { value: true });
 function sum(first, second) {
-    if (typeof first === "number" && typeof second === "number") {
-        return first + second ; 
-    }
-    throw Error("Expected two numbers");
+    return first + second;
 }
-
 let result = sum(10, 10);
 console.log(`Result value: ${result}, Result type: ${typeof result}`);
-
 result = sum(10, "10");
 console.log(`Result value: ${result}, Result type: ${typeof result}`);
-
-// Output
-// Result value: 20, Result type: number
-// ...ch03\primer\index.js:7
-//     throw Error("Expected two numbers");
-//     ^
-
-// Error: Expected two numbers
+// Argument of type 'string' is not assignable to parameter of type 'number'.
