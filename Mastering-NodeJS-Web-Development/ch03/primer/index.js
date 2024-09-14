48e4eb80c1485920c0a45455bf3842ee21c862d8
David Stevenson <drstevenson1958@yahoo.com>
2024-09-14 11:47:21 -0400

    Listing 3.36 Defining a default parameter value in the index.ts file in the primer folder

--- Mastering-NodeJS-Web-Development/ch03/primer/index.js
+++ Mastering-NodeJS-Web-Development/ch03/primer/index.js
@@ -1,11 +1,11 @@
 "use strict";
-// Listing 3.35 Defining an optional parameter in the index.ts file in the primer folder
+// Listing 3.36 Defining a default parameter value in the index.ts file in the primer folder
 Object.defineProperty(exports, "__esModule", { value: true });
-function writeValue(val) {
-    console.log(`Value: ${val ?? "Fallback value"}`);
+function writeValue(val = "default value") {
+    console.log(`Value: ${val}`);
 }
 writeValue("London");
 writeValue();
 // Output
 // Value: London
-// Value: Fallback value
+// Value: default value
