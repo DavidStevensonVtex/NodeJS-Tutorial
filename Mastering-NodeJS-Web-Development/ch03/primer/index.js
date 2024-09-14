08459b47d1f158781364c184db67230ffb63a1f5
David Stevenson <drstevenson1958@yahoo.com>
2024-09-14 11:12:04 -0400

    Listing 3.20 Defining boolean values in the index.ts file in the primer folder

--- Mastering-NodeJS-Web-Development/ch03/primer/index.js
+++ Mastering-NodeJS-Web-Development/ch03/primer/index.js
@@ -1,16 +1,5 @@
 "use strict";
-// Listing 3.19 Using a type union in the index.ts file in the primer folder
+// Listing 3.20 Defining boolean values in the index.ts file in the primer folder
 Object.defineProperty(exports, "__esModule", { value: true });
-let condition = true;
-let person = "Bob";
-const age = 40;
-let place;
-console.log("Place value: " + place + " Type: " + typeof (place));
-place = "London";
-console.log("Place value: " + place + " Type: " + typeof (place));
-place = null;
-console.log("Place value: " + place + " Type: " + typeof (place));
-// Output
-// Place value: undefined Type: undefined
-// Place value: London Type: string
-// Place value: null Type: object  
+let firstBool = true;
+let secondBool = false;
