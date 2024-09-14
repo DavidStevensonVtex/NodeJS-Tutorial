a5f33b16747e0a7bc6dc9bb6353a7a103db457fe
David Stevenson <drstevenson1958@yahoo.com>
2024-09-14 10:29:35 -0400

    Listing 3.13 Changing the function in the index.ts file in the primer folder
    Operator '+' cannot be applied to types 'number' and 'string | number'.

--- Mastering-NodeJS-Web-Development/ch03/primer/index.js
+++ Mastering-NodeJS-Web-Development/ch03/primer/index.js
@@ -1,18 +1,12 @@
 "use strict";
-// Listing 3.12 Using a type union in the index.ts file in the primer folder
+// Listing 3.13 Changing the function in the index.ts file in the primer folder
 Object.defineProperty(exports, "__esModule", { value: true });
 function sum(first, second) {
-    if (typeof second === "string") {
-        return first + Number.parseInt(second);
-    }
-    else {
-        return first + second;
-    }
+    return first + second;
 }
 let result = sum(10, 10);
 console.log(`Result value: ${result}, Result type: ${typeof result}`);
 result = sum(10, "10");
 console.log(`Result value: ${result}, Result type: ${typeof result}`);
-// Output
-// Result value: 20, Result type: number
-// Result value: 20, Result type: number
+// Problem
+// Operator '+' cannot be applied to types 'number' and 'string | number'.
