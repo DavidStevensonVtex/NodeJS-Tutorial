cf0bd0d80c887edc9203c27ba8d7e11c028b0d8d
David Stevenson <drstevenson1958@yahoo.com>
2024-09-14 11:13:27 -0400

    Listing 3.21 Defining string variables in the index.ts file in the primer folder

--- Mastering-NodeJS-Web-Development/ch03/primer/index.js
+++ Mastering-NodeJS-Web-Development/ch03/primer/index.js
@@ -1,5 +1,5 @@
 "use strict";
-// Listing 3.20 Defining boolean values in the index.ts file in the primer folder
+// Listing 3.21 Defining string variables in the index.ts file in the primer folder
 Object.defineProperty(exports, "__esModule", { value: true });
-let firstBool = true;
-let secondBool = false;
+let firstSTring = "This is a string";
+let secondString = 'And so is this';
