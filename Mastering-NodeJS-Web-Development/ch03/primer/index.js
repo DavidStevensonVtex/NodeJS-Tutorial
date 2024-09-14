932f5cea12483b4901c7d6762f56439b680037b1
David Stevenson <drstevenson1958@yahoo.com>
2024-09-14 11:24:32 -0400

    Listing 3.27 Using the identity operator in the index.ts file in the primer folder

--- Mastering-NodeJS-Web-Development/ch03/primer/index.js
+++ Mastering-NodeJS-Web-Development/ch03/primer/index.js
@@ -1,13 +1,13 @@
 "use strict";
-// Listing 3.26 Using the equality operator in the index.ts file in the primer folder
+// Listing 3.27 Using the identity operator in the index.ts file in the primer folder
 Object.defineProperty(exports, "__esModule", { value: true });
 let firstVal = 5;
 let secondVal = "5";
-if (firstVal == secondVal) {
+if (firstVal === secondVal) {
     console.log("They are the same");
 }
 else {
     console.log("They are NOT the same");
 }
 // Output
-// They are the same
+// They are NOT the same
