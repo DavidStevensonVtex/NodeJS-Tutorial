1ddb6d2cc0f134c03d589da6dd849cbe0f429023
David Stevenson <drstevenson1958@yahoo.com>
2024-09-14 11:22:53 -0400

    Listing 3.26 Using the equality operator in the index.ts file in the primer folder

--- Mastering-NodeJS-Web-Development/ch03/primer/index.js
+++ Mastering-NodeJS-Web-Development/ch03/primer/index.js
@@ -1,27 +1,13 @@
 "use strict";
-// Listing 3.25 Using the if/else and switch conditional statements in the index.ts file in the primer folder
+// Listing 3.26 Using the equality operator in the index.ts file in the primer folder
 Object.defineProperty(exports, "__esModule", { value: true });
-let firstName = "Adam";
-if (firstName == "Adam") {
-    console.log("firstName is Adam");
-}
-else if (firstName == "Jacqui") {
-    console.log("firstName is Jacqui");
+let firstVal = 5;
+let secondVal = "5";
+if (firstVal == secondVal) {
+    console.log("They are the same");
 }
 else {
-    console.log("firstName is neither Adam or Jacqui");
-}
-switch (firstName) {
-    case "Adam":
-        console.log("firstName is Adam");
-        break;
-    case "Jacqui":
-        console.log("firstName is Jacqui");
-        break;
-    default:
-        console.log("firstName is neither Adam or Jacqui");
-        break;
+    console.log("They are NOT the same");
 }
 // Output
-// firstName is Adam
-// firstName is Adam
+// They are the same
