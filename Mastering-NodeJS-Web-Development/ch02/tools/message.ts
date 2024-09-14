5f4dfae56e14c3328781b9dbc710d50311950731
David Stevenson <drstevenson1958@yahoo.com>
2024-09-14 09:31:23 -0400

    Listing 2.11 The contents of the message.ts file in the tools folder.

---
+++ Mastering-NodeJS-Web-Development/ch02/tools/message.ts
@@ -0,0 +1,7 @@
+// Listing 2.11 The contents of the message.ts file in the tools folder.
+
+function writeMessage(msg: string) {
+    console.log(`Message: ${msg}`);
+}
+
+writeMessage("This is the message");
