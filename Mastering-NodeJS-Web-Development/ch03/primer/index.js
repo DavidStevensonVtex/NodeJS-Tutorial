69c84649d7d7f1c261e0d489b348b71480eaef9a
David Stevenson <drstevenson1958@yahoo.com>
2024-09-14 11:39:41 -0400

    Listing 3.32 Guarding against null or undefined values in the index.ts file in the primer folder

--- Mastering-NodeJS-Web-Development/ch03/primer/index.js
+++ Mastering-NodeJS-Web-Development/ch03/primer/index.js
@@ -1,16 +1,10 @@
 "use strict";
-// Listing 3.31 Using the nullish coalescing operator in the index.ts file in the primer folder
+// Listing 3.32 Guarding against null or undefined values in the index.ts file in the primer folder
 Object.defineProperty(exports, "__esModule", { value: true });
-let val1;
-let val2 = "London";
-let val3 = 0;
-let coalesced1 = val1 || "fallback value";
-let coalesced2 = val2 || "fallback value";
-let coalesced3 = val3 || 100;
-console.log(`Result 1: ${coalesced1}`);
-console.log(`Result 2: ${coalesced2}`);
-console.log(`Result 3: ${coalesced3}`);
+let count = 100;
+if (count != null && count != undefined) {
+    let result1 = count.toFixed(2);
+    console.log(`Result 1: ${result1}`);
+}
 // Output
-// Result 1: fallback value
-// Result 2: London
-// Result 3: 100  
+// Result 1: 100.00
