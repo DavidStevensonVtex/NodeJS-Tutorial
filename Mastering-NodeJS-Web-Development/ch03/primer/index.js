d43883843cb4f30d4dadad09b9af2451bf06f1e8
David Stevenson <drstevenson1958@yahoo.com>
2024-09-14 11:30:17 -0400

    Listing 3.29 Using the logical OR operator(||) instead of the null coalescing operator in the index.ts file in the primer folder

--- Mastering-NodeJS-Web-Development/ch03/primer/index.js
+++ Mastering-NodeJS-Web-Development/ch03/primer/index.js
@@ -1,13 +1,12 @@
 "use strict";
-// Listing 3.28 Removing the type annotations in the index.ts file in the primer folder
+// Listing 3.29 Using the logical OR operator(||) instead of the null coalescing operator in the index.ts file in the primer folder
 Object.defineProperty(exports, "__esModule", { value: true });
-let firstVal = 5;
-let secondVal = "5";
-if (firstVal === secondVal) {
-    console.log("They are the same");
-}
-else {
-    console.log("They are NOT the same");
-}
-// Problems
-// index.ts(6,5): error TS2367: This comparison appears to be unintentional because the types 'number' and 'string' have no overlap.
+let val1;
+let val2 = "London";
+let coalesced1 = val1 || "fallback value";
+let coalesced2 = val2 || "fallback value";
+console.log(`Result 1: ${coalesced1}`);
+console.log(`Result 2: ${coalesced2}`);
+// Output
+// Result 1: fallback value
+// Result 2: London
