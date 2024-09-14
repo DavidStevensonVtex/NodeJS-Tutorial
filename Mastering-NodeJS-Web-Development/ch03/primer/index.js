d0a84c537c64dbe44ab535787a005ae384834d30
David Stevenson <drstevenson1958@yahoo.com>
2024-09-14 11:32:47 -0400

    Listing 3.30 An unexpected null coalescing result in the index.ts file in the primer folder
    Result 3 is an unexpected result when using the || operator.

--- Mastering-NodeJS-Web-Development/ch03/primer/index.js
+++ Mastering-NodeJS-Web-Development/ch03/primer/index.js
@@ -1,12 +1,17 @@
 "use strict";
-// Listing 3.29 Using the logical OR operator(||) instead of the null coalescing operator in the index.ts file in the primer folder
+// Listing 3.30 An unexpected null coalescing result in the index.ts file in the primer folder
+// Result 3 is an unexpected result when using the || operator.
 Object.defineProperty(exports, "__esModule", { value: true });
 let val1;
 let val2 = "London";
+let val3 = 0;
 let coalesced1 = val1 || "fallback value";
 let coalesced2 = val2 || "fallback value";
+let coalesced3 = val3 || "fallback value";
 console.log(`Result 1: ${coalesced1}`);
 console.log(`Result 2: ${coalesced2}`);
+console.log(`Result 3: ${coalesced3}`);
 // Output
 // Result 1: fallback value
 // Result 2: London
+// Result 3: fallback value
