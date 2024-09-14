dbb2046c5783ccfa9419110d72dc777e6134b5b0
David Stevenson <drstevenson1958@yahoo.com>
2024-09-14 11:41:55 -0400

    Listing 3.33 Using the optional chaining operator in the index.ts file in the primer folder

--- Mastering-NodeJS-Web-Development/ch03/primer/index.js
+++ Mastering-NodeJS-Web-Development/ch03/primer/index.js
@@ -1,10 +1,8 @@
 "use strict";
-// Listing 3.32 Guarding against null or undefined values in the index.ts file in the primer folder
+// Listing 3.33 Using the optional chaining operator in the index.ts file in the primer folder
 Object.defineProperty(exports, "__esModule", { value: true });
 let count = 100;
-if (count != null && count != undefined) {
-    let result1 = count.toFixed(2);
-    console.log(`Result 1: ${result1}`);
-}
+let result1 = count?.toFixed(2);
+console.log(`Result 1: ${result1}`);
 // Output
 // Result 1: 100.00
