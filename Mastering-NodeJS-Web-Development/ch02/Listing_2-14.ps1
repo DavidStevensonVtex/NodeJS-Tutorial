650acf94dd17bbd577984e99a37554d629fa87d1
David Stevenson <drstevenson1958@yahoo.com>
2024-09-14 09:35:28 -0400

    Listing 2.14 Running a package command
    tsc message.ts

---
+++ Mastering-NodeJS-Web-Development/ch02/Listing_2-14.ps1
@@ -0,0 +1,3 @@
+# Listing 2.14 Running a package command
+
+tsc message.ts
---
+++ Mastering-NodeJS-Web-Development/ch02/tools/message.js
@@ -0,0 +1,5 @@
+// Listing 2.11 The contents of the message.ts file in the tools folder.
+function writeMessage(msg) {
+    console.log("Message: ".concat(msg));
+}
+writeMessage("This is the message");
