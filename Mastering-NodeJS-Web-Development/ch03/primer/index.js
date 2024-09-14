10704acd0bd7108b0680097c7cc6215a22b6653a
David Stevenson <drstevenson1958@yahoo.com>
2024-09-14 10:22:53 -0400

    Listing 3.11 Supporting multiple types in the index.ts file in the primer folder

--- Mastering-NodeJS-Web-Development/ch03/primer/index.js
+++ Mastering-NodeJS-Web-Development/ch03/primer/index.js
@@ -1,11 +1,17 @@
 "use strict";
-// Listing 3.9 The contents of the index.ts file in the primer folder
+// Listing 3.11 Supporting multiple types in the index.ts file in the primer folder
 Object.defineProperty(exports, "__esModule", { value: true });
 function sum(first, second) {
-    return first + second;
+    if (typeof second === "string") {
+        return first + Number.parseInt(second);
+    }
+    else {
+        return first + second;
+    }
 }
 let result = sum(10, 10);
 console.log(`Result value: ${result}, Result type: ${typeof result}`);
 result = sum(10, "10");
 console.log(`Result value: ${result}, Result type: ${typeof result}`);
-// Argument of type 'string' is not assignable to parameter of type 'number'.
+// Problems
+// index.ts(14, 18): error TS2345: Argument of type 'string' is not assignable to parameter of type 'number'.
