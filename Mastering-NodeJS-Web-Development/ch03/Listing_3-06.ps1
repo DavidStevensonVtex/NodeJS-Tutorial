b2c6d457089b1121cbac167f76ce55dcd69e09ae
David Stevenson <drstevenson1958@yahoo.com>
2024-09-14 10:06:33 -0400

    Listing 3.6 Starting the development tools
    npm run use_js

---
+++ Mastering-NodeJS-Web-Development/ch03/Listing_3-06.ps1
@@ -0,0 +1,15 @@
+# Listing 3.6 Starting the development tools
+
+npm run use_js
+
+# Output
+# > primer@1.0.0 use_js
+# > nodemon
+
+# [nodemon] 2.0.20
+# [nodemon] to restart at any time, enter `rs`
+# [nodemon] watching path(s): *.*
+# [nodemon] watching extensions: js,mjs,json  
+# [nodemon] starting `node index.js`
+# Hello, World
+# [nodemon] clean exit - waiting for changes before restart
