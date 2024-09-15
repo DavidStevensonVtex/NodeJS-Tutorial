6a20daa4454003a705f37445396cddef3a23efeb
David Stevenson <drstevenson1958@yahoo.com>
2024-09-15 12:00:06 -0400

    Listing 3.53 Defining an optional property in the index.ts file in the primer folder

--- Mastering-NodeJS-Web-Development/ch03/primer/index.js
+++ Mastering-NodeJS-Web-Development/ch03/primer/index.js
@@ -1,5 +1,5 @@
 "use strict";
-// Listing 3.52 Adding a property in the index.ts file in the primer folder
+// Listing 3.53 Defining an optional property in the index.ts file in the primer folder
 Object.defineProperty(exports, "__esModule", { value: true });
 let hat = {
     name: "Hat",
@@ -11,10 +11,15 @@ let boots = {
     category: "Snow Gear"
 };
 function printDetails(product) {
-    console.log(`Name: ${product.name}, Price: ${product.price}`);
+    if (product.category !== undefined) {
+        console.log(`Name: ${product.name}, Price: ${product.price}, Category: ${product.category}`);
+    }
+    else {
+        console.log(`Name: ${product.name}, Price: ${product.price}`);
+    }
 }
 printDetails(hat);
 printDetails(boots);
 // Output
 // Name: Hat, Price: 100
-// Name: Boots, Price: 100
+// Name: Boots, Price: 100, Category: Snow Gear
