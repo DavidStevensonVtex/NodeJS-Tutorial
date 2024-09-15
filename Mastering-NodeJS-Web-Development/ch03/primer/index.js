e3fa80bbaf89af1fd9b0cb011398be8330610107
David Stevenson <drstevenson1958@yahoo.com>
2024-09-15 11:42:45 -0400

    Listing 3.49 Using the spread operator in the index.ts file in the primer folder

--- Mastering-NodeJS-Web-Development/ch03/primer/index.js
+++ Mastering-NodeJS-Web-Development/ch03/primer/index.js
@@ -1,17 +1,17 @@
 "use strict";
-// Listing 3.48 Enumerating the contents of an array in the index.ts file in the primer folder
+// Listing 3.49 Using the spread operator in the index.ts file in the primer folder
 Object.defineProperty(exports, "__esModule", { value: true });
 let myArray = [100, "Adam", true];
-for (let i = 0; i < myArray.length; i++) {
-    console.log("Index " + i + ": " + myArray[i]);
-}
-console.log("---");
-myArray.forEach((value, index) => console.log("Index " + index + ": " + value));
+let otherArray = [...myArray, 200, "Bob", false];
+// for (let i = 0; i < myArray.length; i++) {
+//     console.log("Index " + i + ": " + myArray[i]);
+// }
+// console.log("---");
+otherArray.forEach((value, index) => console.log("Index " + index + ": " + value));
 // Output
 // Index 0: 100
 // Index 1: Adam
 // Index 2: true
-// ---
-// Index 0: 100
-// Index 1: Adam
-// Index 2: true
+// Index 3: 200
+// Index 4: Bob
+// Index 5: false
