535dfa5ba3882c8ca5e04518c0093c3b1d5da7cb
David Stevenson <drstevenson1958@yahoo.com>
2024-09-15 11:29:11 -0400

    Listing 3.43 Using a type annotation in the index.ts file in the primer folder

--- Mastering-NodeJS-Web-Development/ch03/primer/index.js
+++ Mastering-NodeJS-Web-Development/ch03/primer/index.js
@@ -1,5 +1,5 @@
 "use strict";
-// Listing 3.42 Creating and populating an array in the index.ts file in the primer folder
+// Listing 3.43 Using a type annotation in the index.ts file in the primer folder
 Object.defineProperty(exports, "__esModule", { value: true });
 let myArray = [];
 myArray[0] = 100;
