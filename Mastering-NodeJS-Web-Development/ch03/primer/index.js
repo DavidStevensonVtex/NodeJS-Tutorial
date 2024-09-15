292c53f4f9f8dace15c5627996b14d063fdafea5
David Stevenson <drstevenson1958@yahoo.com>
2024-09-15 11:32:18 -0400

    Listing 3.45 Populating a new array in the index.ts file in the primer folder

--- Mastering-NodeJS-Web-Development/ch03/primer/index.js
+++ Mastering-NodeJS-Web-Development/ch03/primer/index.js
@@ -1,7 +1,4 @@
 "use strict";
-// Listing 3.44 Restricting array value types in the index.ts file in the primer folder
+// Listing 3.45 Populating a new array in the index.ts file in the primer folder
 Object.defineProperty(exports, "__esModule", { value: true });
-let myArray = [];
-myArray[0] = 100;
-myArray[1] = "Adam";
-myArray[2] = true;
+let myArray = [100, "Adam", true];
