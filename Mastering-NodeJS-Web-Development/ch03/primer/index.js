02508a46363165b021e9f576f8102d26d9893ec1
David Stevenson <drstevenson1958@yahoo.com>
2024-09-15 11:35:58 -0400

    Listing 3.47 Modifying the contents of an array in the index.ts file in the primer folder

--- Mastering-NodeJS-Web-Development/ch03/primer/index.js
+++ Mastering-NodeJS-Web-Development/ch03/primer/index.js
@@ -1,8 +1,9 @@
 "use strict";
-// Listing 3.46 Reading the data from an array in the index.ts file in the primer folder
+// Listing 3.47 Modifying the contents of an array in the index.ts file in the primer folder
 Object.defineProperty(exports, "__esModule", { value: true });
 let myArray = [100, "Adam", true];
+myArray[0] = "Tuesday";
 let val = myArray[0];
 console.log(`Value: ${val}`);
 // Output
-// Value: 100
+// Value: Tuesday
