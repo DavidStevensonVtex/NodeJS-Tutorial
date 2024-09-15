791b2ee6cdf314c8abfc09f7c79999451ce90c82
David Stevenson <drstevenson1958@yahoo.com>
2024-09-15 11:41:02 -0400

    Listing 3.48 Enumerating the contents of an array in the index.ts file in the primer folder

--- Mastering-NodeJS-Web-Development/ch03/primer/index.js
+++ Mastering-NodeJS-Web-Development/ch03/primer/index.js
@@ -1,9 +1,17 @@
 "use strict";
-// Listing 3.47 Modifying the contents of an array in the index.ts file in the primer folder
+// Listing 3.48 Enumerating the contents of an array in the index.ts file in the primer folder
 Object.defineProperty(exports, "__esModule", { value: true });
 let myArray = [100, "Adam", true];
-myArray[0] = "Tuesday";
-let val = myArray[0];
-console.log(`Value: ${val}`);
+for (let i = 0; i < myArray.length; i++) {
+    console.log("Index " + i + ": " + myArray[i]);
+}
+console.log("---");
+myArray.forEach((value, index) => console.log("Index " + index + ": " + value));
 // Output
-// Value: Tuesday
+// Index 0: 100
+// Index 1: Adam
+// Index 2: true
+// ---
+// Index 0: 100
+// Index 1: Adam
+// Index 2: true
