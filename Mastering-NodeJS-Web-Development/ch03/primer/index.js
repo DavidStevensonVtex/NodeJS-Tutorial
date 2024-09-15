3c7de086c787b770e2663cceaf4a1d3624487b86
David Stevenson <drstevenson1958@yahoo.com>
2024-09-15 12:09:43 -0400

    Listing 3.55 Defining a method in the index.ts file in the primer folder

--- Mastering-NodeJS-Web-Development/ch03/primer/index.js
+++ Mastering-NodeJS-Web-Development/ch03/primer/index.js
@@ -1,5 +1,5 @@
 "use strict";
-// Listing 3.54 Defining a class in the index.ts file in the primer folder
+// Listing 3.55 Defining a method in the index.ts file in the primer folder
 Object.defineProperty(exports, "__esModule", { value: true });
 class Product {
     constructor(name, price, category) {
@@ -10,19 +10,26 @@ class Product {
     name;
     price;
     category;
+    printDetails() {
+        if (this.category !== undefined) {
+            console.log(`Name: ${this.name}, Price: ${this.price}, Category: ${this.category}`);
+        }
+        else {
+            console.log(`Name: ${this.name}, Price: ${this.price}`);
+        }
+    }
 }
 let hat = new Product("Hat", 100);
 let boots = new Product("Boots", 100, "Snow Gear");
-function printDetails(product) {
-    if (product.category !== undefined) {
-        console.log(`Name: ${product.name}, Price: ${product.price}, Category: ${product.category}`);
-    }
-    else {
-        console.log(`Name: ${product.name}, Price: ${product.price}`);
-    }
-}
-printDetails(hat);
-printDetails(boots);
+// function printDetails(product: { name: string, price: number, category?: string }) {
+//     if (product.category !== undefined) {
+//         console.log(`Name: ${product.name}, Price: ${product.price}, Category: ${product.category}`);
+//     } else {
+//         console.log(`Name: ${product.name}, Price: ${product.price}`);
+//     }
+// }
+hat.printDetails();
+boots.printDetails();
 // Output
 // Name: Hat, Price: 100
 // Name: Boots, Price: 100, Category: Snow Gear
