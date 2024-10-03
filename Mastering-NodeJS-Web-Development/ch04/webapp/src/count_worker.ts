41f58aeb4780ad74c9d81797cd023426386ec74f
David Stevenson <drsgis@rit.edu>
2024-10-03 14:45:41 -0400

    Listing 4.22 The contents of the count_worker.ts file in src folder

---
+++ Mastering-NodeJS-Web-Development/ch04/webapp/src/count_worker.ts
@@ -0,0 +1,12 @@
+// Listing 4.22 The contents of the count_worker.ts file in src folder
+
+import { workerData, parentPort } from "worker_threads" ;
+
+console.log('Worker thread ${workerData.request} started');
+
+for (let iter = 0 ; iter < workerData.iterations ; iter++ ) {
+    for (let count = 0 ; count < workerData.total  ; count++ ) {
+        count++ ;
+    }
+    parentPort?.postMessage(iter);
+}
